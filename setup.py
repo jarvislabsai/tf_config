ed49e4e62c26b73108be9f2d6d1da628c381a90f
JarvisLabs.ai <60417801+jarvislabsai@users.noreply.github.com>
2021-07-23 11:47:17 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='tf_config',
+      version='0.1',
+      description='API to manage GPU memory',
+      url='#',
+      author='auth',
+      author_email='author@email.com',
+      license='MIT',
+      packages=['tf_config'],
+      zip_safe=False)
