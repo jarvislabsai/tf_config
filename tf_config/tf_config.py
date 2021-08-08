f8daf617b72c061179894d2b426e52c2c63318c7
Vishnu Subramanian <johnfedrickenator@gmail.com>
2021-08-08 17:26:49 +0530

    ch

--- tf_config/tf_config.py
+++ tf_config/tf_config.py
@@ -1,20 +1,18 @@
 import os
 import tensorflow as tf
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"]="true"
-def tf_config():
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+
+
+def tf_config(sz=7):
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
       # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
         try:
             tf.config.experimental.set_virtual_device_configuration(
                 gpus[0],
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*12)])
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*int(sz))])
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            
+
         except RuntimeError as e:
             # Virtual devices must be set before GPUs have been initialized
             pass
-
-tf_config()
-        
-
